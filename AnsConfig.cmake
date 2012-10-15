7cc5ce6876c0b488a56e0bc7dbc7bbe1fdcb2996
answeror <answeror@gmail.com>
2012-10-15 23:17:05 +0800

    use relative path in config.cmake

--- AnsConfig.cmake
+++ AnsConfig.cmake
@@ -1,9 +1,3 @@
 # Just include ANS_INCLUDE_DIRS.
 
-if(Ans_DIR)
-    set(ANS_INSTALL_DIR "${Ans_DIR}/include")
-else()
-    message(FATAL_ERROR "Can't find a ANS installation.")
-endif()
-
-set(ANS_INCLUDE_DIRS "${ANS_INSTALL_DIR}")
+set(ANS_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include")
